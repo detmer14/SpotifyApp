6b642c3c457d311d470981817d4196e3fac6103d
detmer14 <detmer14@gmail.com>
2026-01-22 14:55:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+const MOCK_MODE = true
+
+const playlists = [
+    {id: "A", trackCount: 120},
+    {id: "B", trackCount: 80},
+    {id: "C", trackCount: 40},
+]
+
+
+async function initializePlayer(){
+    window.onSpotifyWebPlaybackSDKReady = () => {
+        const player = new Spotify.Player({
+            name: 'Rndom Playlist Player',
+            getOAuthToken: cb => cb(accessToken),
+            volume: 0.8
+        })
+
+        player.connect()
+    }
+}
+
+async function pickRandomSong(){
+    const total = playlists.reduce((sum, p) => sum + p.trackCount, 0)
+    const randomindex = Math.floor(Math.random() * total)
+
+    let cumulative = 0
+    let chosenplaylist, index
+    for (const playlist of playlists) {
+        if (randomindex < cumulative + playlist.trackCount) {
+            chosenplaylist = playlist
+            index = randomindex - cumulative
+            alert('Playlist ${p.id}, song #${index + 1}')
+            return
+        }
+        cumulative += playlist.trackCount
+    }
+
+    const track = await getTrackAtIndex(accessToken, chosenplaylist.id, index)
+        playTrack(track.url)
+}
+
+
+
+
+
+async function getTrackAtIndex(token, playlistId, index){
+    const limit = 1
+    const offset = index
+
+    const res = await fetch(
+
+'https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=${limit}&offset=${offset}',
+        {
+            headers: { Authorizatoin: 'Bearer ${token}' }
+        }
+    )
+
+    const data = await res.json()
+    return data.items[0].track
+}
+
+
+async function playTrack(trackUri){
+    await fetch(
+        'https://api.spotify.com/v1/me/player/play',
+        {
+            method: 'PUT',
+            headers: {
+                Authorization: 'Bearer ${accessToken}', 'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                uris: [trackUri]
+            })
+        }
+    )
+}
+
+
+document.getElementById('pick').onclick = pickRandomSong
